895499a152f2fc10b08e92151990abf3a3d7e33c
Isaac Peña <info@ipenat.com>
2025-12-23 12:19:00 +0100

    Add exercise week 1

---
+++ 1_foundations/test_pushover.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import json
+import os
+import requests
+
+load_dotenv(override=True)
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+
+push("Prueba de envío de notificación")
